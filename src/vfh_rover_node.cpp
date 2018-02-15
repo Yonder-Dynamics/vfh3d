d784acad5f3b06f4e434d54fd3ff9174179281f7
Alex Mai <alexandertmai@gmail.com>
2018-02-14 23:37:28 -0800

    Fixed segfault

--- src/vfh_rover_node.cpp
+++ src/vfh_rover_node.cpp
@@ -10,7 +10,7 @@ int main(int argc, char ** argv) {
   ros::init(argc, argv, "vfh_rover_node");
   ros::NodeHandle n;
   Vehicle v = {0,0,1, 1,1,1, 2};
-  OctomapProcessing op (M_PI/10, v, 20, n);
+  OctomapProcessing op (M_PI/10, v, 5, n);
   ros::Subscriber sub = n.subscribe("/octomap_full", 3,
       &OctomapProcessing::octomapCallback, &op);
   ros::spin();
