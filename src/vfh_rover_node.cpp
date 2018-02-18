4cf585f60c08cbe5b7a7e73d65dadab4de07ec79
Alex Mai <alexandertmai@gmail.com>
2018-02-18 14:38:47 -0800

    Minor tweaks

--- src/vfh_rover_node.cpp
+++ src/vfh_rover_node.cpp
@@ -9,7 +9,7 @@ using namespace octomap;
 int main(int argc, char ** argv) {
   ros::init(argc, argv, "vfh_rover_node");
   ros::NodeHandle n;
-  Vehicle v = {0,0,1, 1,1,1, 2};
+  Vehicle v = {0,0,6, 1,1,1, 2};
   OctomapProcessing op (M_PI/10, v, 5, n);
   ros::Subscriber sub = n.subscribe("/octomap_full", 3,
       &OctomapProcessing::octomapCallback, &op);
