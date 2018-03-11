3bd8f9f2e70a0972470e01019643682281e07b74
Nabi <nozberkm@ucsd.edu>
2018-03-11 15:35:34 -0700

    Corrected merge

--- src/OctomapProcessing.cpp
+++ src/OctomapProcessing.cpp
@@ -15,16 +15,16 @@ bool HAS_PROC = false; // temp var, only process once. For debugging
 static int COUNT = 0;
 
 OctomapProcessing::OctomapProcessing(float alpha, Vehicle v,
-        float maxRange, ros::NodeHandle n) :
-    vehicle(v), maxRange(maxRange), gotGoal(false), gotOcto(false), alpha(alpha), prevPose(NULL)
+    float maxRange, ros::NodeHandle n) :
+  vehicle(v), maxRange(maxRange), gotGoal(false), gotOcto(false), alpha(alpha), prevPose(NULL)
 {
-    histogram_pub = n.advertise<sensor_msgs::PointCloud2>("histogram", 2);
-    pose_pub = n.advertise<geometry_msgs::PoseArray>("open_poses", 2);
-    next_pose_pub = n.advertise<geometry_msgs::PoseStamped>("next_direction", 2);
+  histogram_pub = n.advertise<sensor_msgs::PointCloud2>("histogram", 2);
+  pose_pub = n.advertise<geometry_msgs::PoseArray>("open_poses", 2);
+  next_pose_pub = n.advertise<geometry_msgs::PoseStamped>("next_direction", 2);
 }
 
 void OctomapProcessing::poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-    vehicle.setPose(msg->pose);
+  vehicle.setPose(msg->pose);
 }
 
 void OctomapProcessing::goalCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
@@ -36,45 +36,36 @@ void OctomapProcessing::goalCallback(const geometry_msgs::PoseStamped::ConstPtr&
 }
 
 void OctomapProcessing::octomapCallback(const octomap_msgs::Octomap::ConstPtr& msg) {
-<<<<<<< HEAD
-    std::cout << "Got octomap" << std::endl;
-    octomap::AbstractOcTree* atree = octomap_msgs::msgToMap(*msg);
-    tree = boost::make_shared<octomap::OcTree>(*(octomap::OcTree *)atree);
-    gotOcto = true;
-    if (not HAS_PROC && gotGoal) {
-        process();
-    }
-    delete(tree);
-=======
+  std::cout << "Got octomap" << std::endl;
   octomap::AbstractOcTree* atree = octomap_msgs::msgToMap(*msg);
   tree = boost::make_shared<octomap::OcTree>(*(octomap::OcTree *)atree);
   gotOcto = true;
   if (not HAS_PROC && gotGoal) {
     process();
   }
->>>>>>> 0cc02da3d7810a3d726df631fe157902f31ace9f
+  delete(tree);
 }
 
 void OctomapProcessing::simulate() {
-    HAS_PROC = true;
-    for (float x=-5; x<15; x+=0.5) {
-        //vehicle.x = x;
-        process();
-        usleep(1000*1000*1000);
-    }
+  HAS_PROC = true;
+  for (float x=-5; x<15; x+=0.5) {
+    //vehicle.x = x;
+    process();
+    usleep(1000*1000*1000);
+  }
 }
 
 void OctomapProcessing::process() {
-    // Build
-    VFHistogram h (tree, vehicle, maxRange, alpha);
-    h.binarize(1);
+  // Build
+  VFHistogram h (tree, vehicle, maxRange, alpha);
+  h.binarize(1);
 
-    COUNT++;
-    if(COUNT == 10)
-        exit(0);
-    // Disp string
-    //std::cout << h.displayString() << std::endl;
-    // Disp histogram point cloud
+  COUNT++;
+  if(COUNT == 10)
+    exit(0);
+  // Disp string
+  //std::cout << h.displayString() << std::endl;
+  // Disp histogram point cloud
 
   RGBPointCloud::Ptr pc = h.displayCloud(1);
   sensor_msgs::PointCloud2 pc_msg;
